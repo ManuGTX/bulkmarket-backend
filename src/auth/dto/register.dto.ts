c16e8cef2ba0d91a2adaf4efc2f814543ecef179
ManuGTX <manu.terrado@gmail.com>
2026-09-11 19:10:19 -0300

    Change a couple of users

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -5,7 +5,7 @@ import { RolCliente } from '../../users/user.entity';
 class RegistroNegocioDto {
   @IsString() @IsNotEmpty() razonSocial: string;
   @IsString() @IsNotEmpty() nombreComercial: string;
-  @IsString() @Matches(/^[A-Za-z0-9-]{6,50}$/) identificacionFiscal: string;
+  @IsString() @Matches(/^\d{11}$/) identificacionFiscal: string;
   @IsString() @IsNotEmpty() telefono: string;
   @IsString() @IsNotEmpty() direccion: string;
 }
--- src/business/dto/update-business.dto.ts
+++ src/business/dto/update-business.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
 export class ActualizarNegocioDto {
   @IsOptional() @IsString() @IsNotEmpty() razonSocial?: string;
   @IsOptional() @IsString() @IsNotEmpty() nombreComercial?: string;
-  @IsOptional() @IsString() @Matches(/^[A-Za-z0-9-]{6,50}$/) identificacionFiscal?: string;
+  @IsOptional() @IsString() @Matches(/^\d{11}$/) identificacionFiscal?: string;
   @IsOptional() @IsString() @IsNotEmpty() telefono?: string;
   @IsOptional() @IsString() @IsNotEmpty() direccion?: string;
 }
